61fb5662337e2b14bd6a6e4b422c8bf6c12d35bd
Grace <gsj33@cornell.edu>
2025-07-26 23:18:06 -0700

    add dielectric shader

--- material.h
+++ material.h
@@ -47,18 +47,49 @@ private:
 class metal : public material
 {
 public:
-    metal(const color &albedo) : albedo(albedo) {}
+    metal(const color &albedo, double fuzz) : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {} // albedo property and fuzz it
 
     bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered)
         const override
     {
         vec3 reflected = reflect(r_in.direction(), rec.normal);
+        // fuzz is just randomizing the reflectd riection by using a small sphere and chooosing a new endpoint for the ray.
+        //  fuzz has to be scald compard to reflectio nreflector, so just normalize reflected ray
+        reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
+
         scattered = ray(rec.p, reflected);
         attenuation = albedo;
-        return true;
+
+        return (dot(scattered.direction(), rec.normal) > 0);
     }
 
 private:
     color albedo;
+    double fuzz;
+};
+
+// dielectric material that always reracs
+class dielectric : public material
+{
+public:
+    dielectric(double refraction_index) : refraction_index(refraction_index) {}
+
+    bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered)
+        const override
+    {
+        attenuation = color(1.0, 1.0, 1.0);
+        double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        vec3 refracted = refract(unit_direction, rec.normal, ri);
+
+        scattered = ray(rec.p, refracted);
+        return true;
+    }
+
+private:
+    // refractive index in vacuum or air, or the ratio of the material's refractive index over
+    // the refractive index of the enclosing media
+    double refraction_index;
 };
 #endif
